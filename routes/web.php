4a77a493db0868f24a3711deb3d244ba08d71e44
tyoo-18 <bukanbowo123@gmail.com>
2024-04-22 09:31:29 +0700

    menambahkan controller dan routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use \App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
 
 //route resource
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
+
+Route::get('/home', [HomeController::class, 'index']);
